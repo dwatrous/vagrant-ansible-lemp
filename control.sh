1a12180f3ebe922f8a541016310b5c2a61802640
Daniel Watrous <dwmaillist@gmail.com>
2014-07-28 11:25:32 -0500

    add update call in apt-get

--- control.sh
+++ control.sh
@@ -5,6 +5,7 @@
 #export https_proxy=https://proxy.example.com:8080
 
 # bootstarp ansible for convenience on the control box
+apt-get update
 apt-get -y install git python-dev python-pip
 pip install ansible
 
